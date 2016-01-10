48b90538f26d86ad86dda35765969c1c2ee44553
Nick Brown <nickbrown714@gmail.com>
2016-01-09 16:54:37 -0800

    Course course_num method

--- app/models/course.rb
+++ app/models/course.rb
@@ -1,2 +1,5 @@
 class Course < ActiveRecord::Base
+  def course_num
+    self[:course_num]
+  end
 end
--- spec/models/course_spec.rb
+++ spec/models/course_spec.rb
@@ -1,5 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe Course, :type => :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let (:course_instance) { Course.create(dept: "Math", num: "3A", desc: "Calculus") }
+  describe "public instance methods" do
+    context "responds to its methods" do
+      it { expect(course_instance).to respond_to(:course_num) }
+    end
+  end
 end
