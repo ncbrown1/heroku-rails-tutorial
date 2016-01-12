8ca04e9f16794fac4d4b3df138677d13f35bd5de
Nick Brown <nickbrown714@gmail.com>
2016-01-12 10:19:36 -0800

    proper ruby env

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,6 +1,6 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :github, env['GITHUB_KEY'], env['GITHUB_SECRET'], scope: "user,repo,gist"
-  provider :google_oauth2, env['GOOGLE_CLIENT_ID'], env['GOOGLE_CLIENT_SECRET'],
+  provider :github, ENV['GITHUB_KEY'], ENV['GITHUB_SECRET'], scope: "user,repo,gist"
+  provider :google_oauth2, ENV['GOOGLE_CLIENT_ID'], ENV['GOOGLE_CLIENT_SECRET'],
     scope: 'profile', image_aspect_ratio: 'square', image_size: 48, access_type: 'online', name: 'google'
 end
 
